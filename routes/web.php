cc9521200970e149c7f313d8ce45480db17e49f2
danielSzkotak <daniel.szkotak.programista@gmail.com>
2025-02-06 20:57:38 +0100

    Basic form and submit actions 2

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Models\Listing;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[IndexController::class, 'index']);
 Route::get('hello',[IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class)
-->only(['index', 'show']);
+->only(['index', 'show', 'create', 'store']);
+
+
